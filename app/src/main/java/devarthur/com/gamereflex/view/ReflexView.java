cc50743a16df4e77e3a0bb776e20141aa9fcdc00
Unknown <arthur.gomes_4718@hotmail.com>
2018-11-05 20:22:17 -0200

    Reflex View Class

---
+++ app/src/main/java/devarthur/com/gamereflex/view/ReflexView.java
@@ -0,0 +1,18 @@
+package devarthur.com.gamereflex.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Arthur on 05/11/2018.
+ */
+
+public class ReflexView extends View {
+    public ReflexView(Context context) {
+        super(context);
+    }
+
+
+}
