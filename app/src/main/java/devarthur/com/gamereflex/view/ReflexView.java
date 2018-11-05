e256db13ba972ea8b3f2dbb2132b3d96022d688e
Unknown <arthur.gomes_4718@hotmail.com>
2018-11-05 20:24:07 -0200

    Reflex Class Constructor
    Added a code to the class, the constructor now receiveis de sharedprefereces and the layouts. The preferences are in order to create a view with a game state in mind. The layout its to enable the app to change its view.

--- app/src/main/java/devarthur/com/gamereflex/view/ReflexView.java
+++ app/src/main/java/devarthur/com/gamereflex/view/ReflexView.java
@@ -1,16 +1,17 @@
 package devarthur.com.gamereflex.view;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
-import android.util.AttributeSet;
+import android.content.SharedPreferences;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Arthur on 05/11/2018.
  */
 
 public class ReflexView extends View {
-    public ReflexView(Context context) {
+
+    public ReflexView(Context context, SharedPreferences sharedPreferences, RelativeLayout parentLayout) {
         super(context);
     }
 
