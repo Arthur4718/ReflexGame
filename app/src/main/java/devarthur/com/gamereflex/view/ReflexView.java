0c81f964025711006d2328b000ca02a4f98ebdeb
Unknown <arthur.gomes_4718@hotmail.com>
2018-11-05 20:39:10 -0200

    Member Variables.

--- app/src/main/java/devarthur/com/gamereflex/view/ReflexView.java
+++ app/src/main/java/devarthur/com/gamereflex/view/ReflexView.java
@@ -10,6 +10,22 @@ import android.widget.RelativeLayout;
  */
 
 public class ReflexView extends View {
+    //Contants;
+    private static final String HIGH_SCORE = "HIGH_SCORE";
+    private SharedPreferences preferences;
+
+    //Variables that manage the game
+    private int spotsTouched;
+    private int score;
+    private int level;
+    private int viewWidth;
+    private int viewHeight;
+    private int animationTime;
+    private boolean gameOver;
+    private boolean gamePaused;
+    private boolean dialogDisplay;
+    private int highScor; 
+
 
     public ReflexView(Context context, SharedPreferences sharedPreferences, RelativeLayout parentLayout) {
         super(context);
