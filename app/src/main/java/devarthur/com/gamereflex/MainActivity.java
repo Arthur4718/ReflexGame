eb821ed63e69a6c27cc082da4127efe2f4ff776d
Unknown <arthur.gomes_4718@hotmail.com>
2018-11-05 23:36:16 -0200

    Sound pool, files and manager.
    Changed the files in the project files. Added the Sound pool builder to handle audio in the project, added audio preferences and volume.

--- app/src/main/java/devarthur/com/gamereflex/MainActivity.java
+++ app/src/main/java/devarthur/com/gamereflex/MainActivity.java
@@ -1,13 +1,27 @@
 package devarthur.com.gamereflex;
 
+import android.content.Context;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import devarthur.com.gamereflex.view.ReflexView;
 
 public class MainActivity extends AppCompatActivity {
+    private ReflexView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout layout = findViewById(R.id.relativeLayout);
+
+        gameView = new ReflexView(this, getPreferences(Context.MODE_PRIVATE), layout);
+        layout.addView(gameView, 0);
+
+
     }
 }
--- app/src/main/java/devarthur/com/gamereflex/view/ReflexView.java
+++ app/src/main/java/devarthur/com/gamereflex/view/ReflexView.java
@@ -4,17 +4,22 @@ import android.animation.Animator;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
@@ -76,7 +81,7 @@ public class ReflexView extends View {
     public static final int SOUND_PRIORITY = 100;
     public static final int MAX_STREAMS = 4;
     //Stadard tool to handle sounds
-    private SoundPool soundPool;
+    private SoundPool.Builder soundPool;
     private int volume;
     private Map<Integer, Integer> soundMap;
 
@@ -100,21 +105,52 @@ public class ReflexView extends View {
         currentScoreTextView = relativeLayout.findViewById(R.id.score);
         levelTextView = relativeLayout.findViewById(R.id.level_tv);
 
-        spotHandler = new Handler();
+        //spotHandler = new Handler();
+
+        addNewSpot();
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         viewHeight = h;
         viewWidth = w;
+    }
+    private void initializeSoundEffects(Context context){
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .build();
+        soundPool = new SoundPool.Builder();
+        soundPool.setMaxStreams(MAX_STREAMS)
+                .setAudioAttributes(audioAttributes)
+                .build();
+        soundPool.build();
+    
+        AudioManager manager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        volume = manager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        //Sound Map.
+        soundMap = new HashMap<Integer, Integer>();
+        soundMap.put(HIT_SOUND_ID, soundPool.build().load(context, R.raw.hit, SOUND_PRIORITY));
+        soundMap.put(MISS_SOUND_ID, soundPool.build().load(context, R.raw.miss, SOUND_PRIORITY));
+        soundMap.put(DISSAPEAR_SOUND_ID, soundPool.build().load(context, R.raw.dissapear, SOUND_PRIORITY));
 
     }
 
+    private void displayScores(){
+        highscoreTextView.setText(resources.getString(R.string.highscore) + " " + highScore);
+        currentScoreTextView.setText(resources.getString(R.string.score) + " " + score);
+        levelTextView.setText(resources.getString(R.string.level) + " " + level);
+    }
+
+    private Runnable addSpotRunnable = new Runnable() {
+        @Override
+        public void run() {
+            addNewSpot();
+        }
+    };
+
     public void addNewSpot(){
-        int x = random.nextInt(viewWidth - SPOT_DIAMETER);
-        int y = random.nextInt(viewHeight - SPOT_DIAMETER);
-        int x2 = random.nextInt(viewWidth - SPOT_DIAMETER);
-        int y2 = random.nextInt(viewHeight - SPOT_DIAMETER);
 
         //Create the actual spot/cirle
         final ImageView spot = (ImageView) layoutInflater.inflate(R.layout.untouched, null);
@@ -122,12 +158,45 @@ public class ReflexView extends View {
         spots.add(spot);
         spot.setLayoutParams(new RelativeLayout.LayoutParams(SPOT_DIAMETER, SPOT_DIAMETER));
 
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) spot.getContext().getSystemService(Context.WINDOW_SERVICE);
+
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+        int x = random.nextInt(width - SPOT_DIAMETER);
+        int y = random.nextInt(height - SPOT_DIAMETER);
+        int x2 = random.nextInt(width - SPOT_DIAMETER);
+        int y2 = random.nextInt(height - SPOT_DIAMETER);
+
         spot.setImageResource(random.nextInt(2) == 0 ? R.drawable.ic_my_location_green_24dp : R.drawable.ic_my_location_red_24dp);
 
         spot.setX(x);
         spot.setY(y);
 
+        //Check for click event
+        spot.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                touched(spot);
+            }
+        });
+
         relativeLayout.addView(spot); //Put the view on the screen
+    }
+
+    private void touched(ImageView spot) {
+
+        relativeLayout.removeView(spot);
+        //Call the qeue and remove the spot from there
+        spots.remove(spot);
+        level = 1;
+        ++spotsTouched;
+        score += 10 * level;
+
+
+
     }
 
 }
